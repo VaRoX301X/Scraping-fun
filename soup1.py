1c60bc79059e263e6866598e4988954cba807481
Alvaro Recalde <elvarox301@gmail.com>
2019-02-26 20:33:15 +0100

    Initial commit

---
+++ soup1.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def float_or_na(value):
+    return float(value if value != 'N/A' else 'nan')
+
+
+seasons = ['winter','spring','summer','fall']
+for i in range(2017, 2019):
+    for s in seasons:
+        print('------' + s.upper() + '-' + str(i) + '------')
+        html = requests.get('https://myanimelist.net/anime/season/'+ str(i) +'/' + s )
+        soupMAL = BeautifulSoup(html.text, 'lxml')
+        tipoAnimu = soupMAL.findAll('div',
+                                    class_='seasonal-anime-list js-seasonal-anime-list js-seasonal-anime-list-key-1 clearfix')
+        for t in tipoAnimu:
+            divs = t.findAll('div', class_='seasonal-anime js-seasonal-anime')
+            for d in divs:
+                score = float_or_na(d.find('span', class_='score').text.strip())
+                if score >= 7.5:
+                    print(
+                        t.find('div', class_='anime-header').text +
+                        ': ' +
+                        d.find('p', class_='title-text').a.text +
+                        ' | Score: ' +
+                        d.find('span', class_='score').text.strip()
+                    )
